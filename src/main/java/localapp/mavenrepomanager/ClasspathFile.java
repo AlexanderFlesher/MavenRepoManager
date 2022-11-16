db970b7d3773b9f73b65c839b6678d655df01dbc
Alex F <alexjflesher@gmail.com>
2022-11-16 11:04:08 -0700

    added tests and documentation

--- src/main/java/localapp/mavenrepomanager/ClasspathFile.java
+++ src/main/java/localapp/mavenrepomanager/ClasspathFile.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -16,49 +18,55 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+/**
+ * The ClasspathFile class abstracts the Classpath file and records the
+ * classpath entires in a map called {@code ClasspathEntries}. The map is 
+ * indexed by entry kind corresponding to the content of the "kind" 
+ * attribute. The List<String> returned is a list of the contents of the 
+ * "path" attribute for all the entries of the key kind.
+ * <br/><br/>
+ * Throws:<br/><br/>
+ * {@code IllegalArgumentException} when the given file does not exist or
+ * is not in a classpath file format.
+ */
 public final class ClasspathFile {
     private Element root;
-    private String[] jarPaths;
-
+    private HashMap<String, List<String>> classpathEntries;
+    
     public ClasspathFile(InputStream input){
         this.root = getRoot(input);
         if (!validate(root))
             throw new IllegalArgumentException(
                 "The given InputStream is not a classpath file or is incorrectly formatted.");
-        this.jarPaths = generatePaths(root);
 
+        this.classpathEntries = readClasspathEntries(root);
     }
 
     public ClasspathFile(File file){
-        this.root = getRoot(file);
-        if (!validate(root))
-            throw new IllegalArgumentException(
-                String.format("File \"%s\" is not a classpath file or is incorrectly formatted.", file));
         if (!validate(file))
             throw new IllegalArgumentException(
                 String.format("File \"%s\" does not exist.", file));
 
-        this.jarPaths = generatePaths(root);
+        this.root = getRoot(file);
+        if (!validate(root))
+            throw new IllegalArgumentException(
+                String.format("File \"%s\" is not a classpath file or is incorrectly formatted.", file));
+
+        this.classpathEntries = readClasspathEntries(root);
     }
 
     public ClasspathFile(String filepath){
-        this(new File(filepath));
+        this(filepath != null ? new File(filepath) : null);
     }
 
-    public String[] getLibPaths(){
-        return jarPaths;
-    }
-
-    private String[] generatePaths(Element e){
-        List<String> paths = new ArrayList<>();
-        NodeList nodes = e.getElementsByTagName("classpathentry");
-        for (int i = 0; i < nodes.getLength(); i++){
-            Node node = nodes.item(i);
-            if (node.getAttributes().getNamedItem("kind").getTextContent().equals("lib")){
-                paths.add(node.getAttributes().getNamedItem("path").getTextContent());
-            }
-        }
-        return paths.toArray(new String[paths.size()]);
+    /**
+     * 
+     * @return a map that is indexed by entry kind corresponding
+     * to the content of the "kind" attribute. The List<String> returned is a list
+     * of the contents of the "path" attribute for all the entries of the key kind.
+     */
+    public Map<String, List<String>> getClasspathEntries(){
+        return this.classpathEntries;
     }
 
     private Element getRoot(InputStream f){
@@ -87,11 +95,25 @@ public final class ClasspathFile {
         }
     }
 
+    private HashMap<String, List<String>> readClasspathEntries(Element e){
+        HashMap<String, List<String>> map = new HashMap<>();
+        NodeList nodes = e.getElementsByTagName("classpathentry");
+        for (int i = 0; i < nodes.getLength(); i++){
+            Node node = nodes.item(i);
+            final String entryKind = node.getAttributes().getNamedItem("kind").getTextContent();
+            if (!map.containsKey(entryKind))
+                map.put(entryKind, new ArrayList<String>());
+
+            map.get(entryKind).add(node.getAttributes().getNamedItem("path").getTextContent());
+        }
+        return map;
+    }
+
     private boolean validate(Element r){
-        return r == null || !r.getTagName().equals("classpath") ? false : true;
+        return r != null && r.getTagName().equals("classpath");
     }
     
     private boolean validate(File f) {
-        return f.exists();
+        return f != null && f.exists();
     }
 }
--- src/test/java/localapp/mavenrepomanager/ClasspathFileTest.java
+++ src/test/java/localapp/mavenrepomanager/ClasspathFileTest.java
@@ -1,6 +1,7 @@
 package localapp.mavenrepomanager;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.junit.Assert;
@@ -9,16 +10,84 @@ import org.junit.Test;
 
 public class ClasspathFileTest{
     private InputStream file;
+
     @Before
     public void setupFile(){
         file = this.getClass().getResourceAsStream("example.classpath");
-        // file = new File("example.classpath");
     }
 
     @Test
-    public void getPaths(){
-        ClasspathFile test = new ClasspathFile(file);
-        Assert.assertEquals(3, test.getLibPaths().length);
-        // Assert.assertTrue(true);
+    public void getPaths_returnCorrectLibEntries(){
+        final int LIB_ENTRIES = 3;
+        final String LIB_PATH = "path";
+        final String KIND = "lib";
+        ClasspathFile classpath = new ClasspathFile(file);
+        assertCorrectEntries(LIB_ENTRIES, LIB_PATH, KIND, classpath);
+    }
+
+    @Test
+    public void getPaths_returnCorrectSrcEntries(){
+        final int LIB_ENTRIES = 1;
+        final String LIB_PATH = "src";
+        final String KIND = "src";
+        ClasspathFile classpath = new ClasspathFile(file);
+        assertCorrectEntries(LIB_ENTRIES, LIB_PATH, KIND, classpath);
+    }    
+
+    @Test
+    public void getPaths_returnCorrectOutputEntries(){
+        final int LIB_ENTRIES = 1;
+        final String LIB_PATH = "bin";
+        final String KIND = "output";
+        ClasspathFile classpath = new ClasspathFile(file);
+        assertCorrectEntries(LIB_ENTRIES, LIB_PATH, KIND, classpath);
+    }    
+    
+    @Test
+    @SuppressWarnings("unused")
+    public void getPaths_shouldInvalidArgExcept_invalidData(){
+        Assert.assertThrows(IllegalArgumentException.class, 
+            () -> {
+                ClasspathFile classpath = new ClasspathFile(new InputStream() {
+                    @Override
+                    public int read() throws IOException {
+                        return 0;
+                    }
+                });
+            });
+    }
+
+    @Test
+    @SuppressWarnings("unused")
+    public void getPaths_shouldInvalidArgExcept_nullFile(){
+        Assert.assertThrows(IllegalArgumentException.class, 
+            () -> {
+                ClasspathFile classpath = new ClasspathFile((File)null);
+            });
+    }
+
+    @Test
+    @SuppressWarnings("unused")
+    public void getPaths_shouldInvalidArgExcept_nullInput(){
+        Assert.assertThrows(IllegalArgumentException.class, 
+            () -> {
+                ClasspathFile classpath = new ClasspathFile((InputStream)null);
+            });
+    }
+
+    @Test
+    @SuppressWarnings("unused")
+    public void getPaths_shouldInvalidArgExcept_nullPath(){
+        Assert.assertThrows(IllegalArgumentException.class, 
+            () -> {
+                ClasspathFile classpath = new ClasspathFile((String)null);
+            });
+    }
+
+    private void assertCorrectEntries(final int LIB_ENTRIES, final String LIB_PATH, final String KIND, ClasspathFile classpath) {
+        Assert.assertEquals(LIB_ENTRIES, classpath.getClasspathEntries().get(KIND).size());
+        for (String path : classpath.getClasspathEntries().get(KIND)){
+            Assert.assertEquals(LIB_PATH, path);
+        }
     }    
 }
