f40d14347118dad4ebe752204bb688c3c68f8106
Alexander Flesher <78281043+AlexanderFlesher@users.noreply.github.com>
2022-12-02 15:12:26 -0700

    Better Windows Support (#1)
    * added command line output for debugging
    
    * print os message at the start
    
    * add extra arg for cmd
    
    * fixed tests
    
    * fixed tests
    
    * add extra arg for cmd
    
    * changed default version to 'local' in the case that a version cannot be parsed
    
    * removed unnecessary name resolution method

--- src/main/java/localapp/mavenrepomanager/App.java
+++ src/main/java/localapp/mavenrepomanager/App.java
@@ -9,6 +9,7 @@ public class App
 
     public static void main( String[] args )
     {
+        System.out.println(MavenArgs.getOperatingSystem());
         IRunnable main;
         try{
             App.settings = ArgParser.parse(args);
--- src/main/java/localapp/mavenrepomanager/MavenArgs.java
+++ src/main/java/localapp/mavenrepomanager/MavenArgs.java
@@ -1,9 +1,10 @@
 package localapp.mavenrepomanager;
 
 import java.io.File;
-import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class MavenArgs {
     protected enum OperatingSystem{
@@ -11,7 +12,7 @@ public final class MavenArgs {
         WINDOWS
     }
 
-    private final static String maven = resolveMvn("mvn");
+    private final static String maven = "mvn";
     private final static String command = "org.apache.maven.plugins:maven-install-plugin:2.4:install-file";
     private final static String file = "-Dfile=%s";
     private final static String group = "-DgroupId=%s";
@@ -21,52 +22,38 @@ public final class MavenArgs {
     private final static String repoPath = "-DlocalRepositoryPath=%s";
     
     public static String[] from(Entry entry, RunSettings settings){
-        String[] args = new String[9];
-        args[0] = maven;
-        args[1] = command;
-        args[2] = packaging;
-        args[3] = String.format(file, entry.getDependency().getPath().toString());
-        args[4] = String.format(group, entry.getGroup());
-        args[5] = String.format(artifact, entry.getArtifact());
-        args[6] = String.format(version, entry.getVersion());
-        args[7] = String.format(repoPath, 
-            Path.of(settings.repoPath.toString(), settings.repoName).toString());
-        args[8] = "-X";
-        return args;
+        List<String> args = new ArrayList<>();
+        args.addAll(prependOsArgs(getOperatingSystem())
+            .stream().filter(s -> !s.isBlank()).collect(Collectors.toList()));
+        args.add(maven);
+        args.add(command);
+        args.add(packaging);
+        args.add(String.format(file, entry.getDependency().getPath().toString()));
+        args.add(String.format(group, entry.getGroup()));
+        args.add(String.format(artifact, entry.getArtifact()));
+        args.add(String.format(version, entry.getVersion()));
+        args.add(String.format(repoPath, 
+            Path.of(settings.repoPath.toString(), settings.repoName).toString()));
+        args.add("-X");
+        return args.toArray(new String[args.size()]);
     }
 
     protected static OperatingSystem getOperatingSystem(){
         return File.pathSeparatorChar == ';' ? OperatingSystem.WINDOWS : OperatingSystem.UNIX;
     }
 
-    protected static String resolveMvn(String maven){
-        Collection<String> strings = System.getenv().values();
-        for (String env : strings){
-            for (String path : env.split(File.pathSeparator)){
-                try{
-                    Path combined = Path.of(path, maven);
-                    if (combined.toFile().exists())
-                        return combined.toString();
-                }
-                catch (InvalidPathException ex){
-                    ex.printStackTrace();
-                }
-            }
-        }
-        return prependOsArgs(getOperatingSystem()) + maven;
-    }
-
-    protected static String prependOsArgs(OperatingSystem system){
-        String result;
+    protected static List<String> prependOsArgs(OperatingSystem system){
+        List<String> result = new ArrayList<>();
         switch(system){
             case UNIX:
-                result = "";
+                result.add("");
                 break;
             case WINDOWS:
-                result = "cmd /C ";
+                result.add("cmd"); 
+                result.add("/C");
                 break;
             default:
-                result = "";
+                result.add("");
                 break;
         }
         return result;
